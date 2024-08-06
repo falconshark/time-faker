1b710c4125d2fec71cc9ae9abcc899f8e87eb848
Sardo <sardo.ip@sardo.work>
2024-08-06 23:17:54 +0900

    Init commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Timefaker</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" target="_blank" aria-current="page" href="#">Github</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "bootswatch/dist/flatly/bootstrap.min.css";
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
