88cb753d1485aa1ba69a03f27778fc60f85695e0
Sardo Ip <sardo.ip@sardo.work>
2024-08-12 15:04:18 +0000

    Add github link to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" target="_blank" aria-current="page" href="#">Github</a>
+                            <a class="nav-link" target="_blank" aria-current="page" href="https://github.com/falconshark/time-faker">Github</a>
                         </li>
                     </ul>
                 </div>
